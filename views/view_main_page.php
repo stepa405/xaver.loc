b0d67a028f55994fe5acdf612fe08eaf84c08d09
stepa405 <stepanov405@gmail.com>
2017-04-24 15:29:31 +0300

    Добавил информацию об авторе курса

--- views/view_main_page.php
+++ views/view_main_page.php
@@ -1,8 +1,12 @@
 <?php
-echo 'Привет. Тут я изучаю уроки из Xaver';
-echo 'Тут надо написать про преподавателя';
-echo '<br><br>';
-echo 'view_homeworks_5 <br>';
-echo 'Разобраться как убрать ?page=5 из строк: <br>';
-echo 'form action="index.php?page=5" method="post';
+echo '<h3> Курс от Антона Херсун </h3> ';
+echo '<br>';
+echo '<a href="https://vk.com/topic-79720728_31807766" target="_blank">Бессплатные уроки</a><br>';
+echo '<br>';
+echo '<a href="https://vk.com/xaver" target="_blank">Группа ВКонтакте "Эволюция фриланса"</a><br>';
+echo '<a href="http://xaver.ru/" target="_blank">Официальный сайт</a><br>';
+echo '<a href="http://events.xaver.ru/master/" target="_blank">Мастер группа</a><br>';
+echo '<br>';
+echo '<a href="https://www.youtube.com/channel/UC4vsJ5PMS3qNN61ORVL05og" target="_blank">Ютуб канал</a><br>';
+echo '<a href="https://xaverfreelance.slack.com" target="_blank">Слак канал</a><br>';
 ?>
